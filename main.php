1f202845fd0417ac8c67a87ff92905dc5129aa12
IvanStork <73465123+IvanStork@users.noreply.github.com>
2024-11-14 16:41:08 +0100

    trying a commit

---
+++ main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+
+
+<form action="random.php" method="POST">
+Number of dice: <input type="text" name="number"><br>
+Type of dice: <input type="text" name="dice"><br>
+<input type="submit">
+</form>
+<?php
+
+?>
+
+</body>
+</html>
---
+++ random.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+You chose to roll <?php echo $_GET["number"]; ?> <?php echo $_GET["dice"]; ?><br>
+
+
+</body>
+</html>
